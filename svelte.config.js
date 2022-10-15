8b6e479f407d4107f7cc6158725ccf35d17765b6
Moody0101-X <azmoudhossin0101@gmail.com>
2022-10-15 14:58:12 +0200

    added adapter for netlfy

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from "svelte-preprocess";
-
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	 preprocess: [
+	preprocess: [
     	preprocess({
     		postcss: true,
     	}),
   	],
 	kit: {
-    	adapter: adapter({ out: './build' })
+    	adapter: adapter()
   	}
 };
 
