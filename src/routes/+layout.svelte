9e808e99338f67e30274a4c2a85dab3b65ede58f
Moody0101-X <azmoudhossin0101@gmail.com>
2023-08-24 21:15:06 +0200

    Set up CI/CD and updated the site to provide a web term.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,19 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import './styles.css';
-
-	
-
 </script>
 
 <div class="app reveal w-11/12 lg:w-[1100px] md:w-11/12 sm:w-11/12 flex flex-col items-center justify-center p-0 m-0 poppins">
-	
 	<Header />
 	<main class="w-full m-0 p-0">
 		<slot />
 	</main>
 	<Footer />
-		
 </div>
 
 <style lang="css">
-	
 	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,600;1,100;1,200;1,300;1,400;1,700&display=swap');
-
 	.poppins {
 		font-family: 'Poppins', sans-serif;
 	}
-
 </style>
